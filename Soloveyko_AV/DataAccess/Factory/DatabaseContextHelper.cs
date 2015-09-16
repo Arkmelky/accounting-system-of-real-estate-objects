6240062d3a1fb84af1e4bffa5c4e423f8651ec33
Arkady Soloveyko <arkmelky@mail.ru>
2015-09-16 14:00:20 +0300

    Generic Repo in progress. Signed-off-by: Arkady Soloveyko <arkmelky@mail.ru>

---
+++ Soloveyko_AV/DataAccess/Factory/DatabaseContextHelper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Factory
+{
+    public class DatabaseContextHelper : IDisposable
+    {
+        private static DB_Context dbContext;
+
+        private DatabaseContextHelper()
+        {
+            dbContext = new DB_Context();
+        }
+
+        public static DB_Context GetDbContext()
+        {
+            return new DB_Context();
+        }
+
+        public static void SaveChanges()
+        {
+            dbContext.SaveChanges();
+        }
+
+        public static void Dispose()
+        {
+            dbContext.Dispose();
+        }
+
+        void IDisposable.Dispose()
+        {
+            Dispose();
+        }
+    }
+}
---
+++ Soloveyko_AV/DataAccess/GenericDataManager/GenericDataManager.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+using DataAccess.Factory;
+using DataAccess.GenericDataManager;
+
+namespace DataAccess.GenericDataManager
+{
+    public  abstract class BasicEntity
+    {
+        public int ID { get; set; }}
+    }
+
+
+    public class GenericDataManager<TEntity> where TEntity :  BasicEntity
+    {
+        
+        private DB_Context dbContext;
+
+        public TEntity Get(int id)
+        {
+            return dbContext.Set<TEntity>().Find(id);
+        }
+
+        public void Add(TEntity obj)
+        {
+            dbContext = DatabaseContextHelper.GetDbContext();
+            dbContext.Set<TEntity>().Add(obj);
+            DatabaseContextHelper.SaveChanges();
+            DatabaseContextHelper.Dispose();
+        }
+
+        public void Delete(TEntity obj)
+        {
+            dbContext = DatabaseContextHelper.GetDbContext();
+            dbContext.Set<TEntity>().Remove(obj);
+            DatabaseContextHelper.SaveChanges();
+            DatabaseContextHelper.Dispose();
+        }
+
+        public void Update(TEntity obj, int id)
+        {
+            
+            dbContext = DatabaseContextHelper.GetDbContext();
+            foreach (var o in dbContext.Set<TEntity>())
+            {
+                if (o.ID == obj.ID)
+                {
+                    //o;
+                }
+
+                DatabaseContextHelper.SaveChanges();
+            DatabaseContextHelper.Dispose();
+        }
+
+    }
+}
